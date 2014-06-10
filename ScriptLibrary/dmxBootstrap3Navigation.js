83f362b5e71bf0ef071fc5dc48503e6a52509ab0
Miroslav Zografski <mzografski@gmail.com>
2014-06-10 10:39:44 +0300

    removed unwhanted files, adde some imagery and a main css file.

--- ScriptLibrary/dmxBootstrap3Navigation.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($){
-	var url = window.location.href;
-	$('ul.nav a').map(function() {
- 		$(this).parent().toggleClass('active', this.href == url || this.href == url.split("?")[0].split("#")[0]);
-	});
-});
